cc01cefa6df8f3f3b2cf9d54391801bf99e1731e
ABHISHEK <ABHISHEK>
2023-03-30 23:08:03 +0530

    connect button

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,8 +1,45 @@
-
-
+'use client'
+import { useEffect } from "react";
+import { useMoralis } from "react-moralis";
 function Header(){
+    const {enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3, isWeb3EnableLoading} = useMoralis();
+    useEffect(() => {
+        if(isWeb3Enabled)
+            return
+        if(window !== "undefined"){
+            if(window.localStorage.getItem("connected")){
+                enableWeb3();
+            }
+        }
+        // return () => {
+        //     cleanup
+        // }
+    }, [isWeb3Enabled])
+
+    useEffect(() => {
+        Moralis.onAccountChanged((account)=>{
+            console.log(`Account changed ${account}`);
+            if(account == null){
+                window.localStorage.removeItem("connected")
+                deactivateWeb3()
+            }
+        })
+        return () => {
+            cleanup
+        }
+    }, [])
     return(<div>
-        Header
+        {account?(<div>
+            Connected to {account}
+        </div>):(<div>
+            <button onClick={async ()=>{
+                await enableWeb3()
+                if(window !== "undefined"){
+                    window.localStorage.setItem("connected", "connectedToWallet")
+                }
+            }}
+            disabled={isWeb3EnableLoading}>Connect</button>
+        </div>)}
     </div>)
 }
 
--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
+'use client'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import Header from '@/Components/Header'
 
+import { MoralisProvider } from 'react-moralis'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+  <MoralisProvider initializeOnMount={false}>
+      <main className={styles.main}>
       <Header></Header>
       <div className={styles.description}>
         <p>
@@ -19,5 +23,6 @@ export default function Home() {
           <p>hello</p>
       </div>
     </main>
+  </MoralisProvider>
   )
 }
